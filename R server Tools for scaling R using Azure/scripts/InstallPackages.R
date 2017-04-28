bb1c475139a2354f6d7b0c8d9d8ca15e469bfede
Vanja Paunic <vapaunic@microsoft.com>
2017-04-28 16:24:24 -0700

    modified dplyr rmd

--- R server Tools for scaling R using Azure/scripts/InstallPackages.R
+++ R server Tools for scaling R using Azure/scripts/InstallPackages.R
@@ -1,5 +1,6 @@
 install.packages("broom", repos='http://cran.us.r-project.org')
 install.packages("AzureML", repos='http://cran.us.r-project.org')
+install.packages("ggplot2", repos='http://cran.us.r-project.org')
 
 install.packages("devtools")
 library(devtools)
